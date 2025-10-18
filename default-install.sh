1a14d4c5c4f8cead1f51ddc27be008044644c971
charli-edna-dow <charli.edna.dow@gmail.com>
2025-10-18 15:47:54 +0000

    Update default-install.sh

--- default-install.sh
+++ default-install.sh
@@ -1,1 +1,1 @@
-sudo pacman -S xorg-server lxqt breeze-icons networkmanager man nano gnome evolution pipewire wireplumber pipewire-jack pipewire-pulse noto-fonts noto-fonts-cjk noto-fonts-emoji firefox libreoffice gcc htop
+sudo pacman -S xorg-server lxqt breeze-icons man nano gnome evolution pipewire wireplumber pipewire-jack pipewire-pulse noto-fonts noto-fonts-cjk noto-fonts-emoji firefox libreoffice gcc htop
