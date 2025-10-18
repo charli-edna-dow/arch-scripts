32d076adc6d7a5c92d246457be476742cfd4c723
charli-edna-dow <charli.edna.dow@gmail.com>
2025-10-18 16:01:58 +0000

    Update pacstrap-install-XE310XBA.sh

--- pacstrap-install-XE310XBA.sh
+++ pacstrap-install-XE310XBA.sh
@@ -1,1 +1,1 @@
-pacstrap -K /mnt base linux linux-firmware networkmanager intel-ucode mesa vulkan-intel xf86-video-intel xf86-video-fbdev
+pacstrap -K /mnt base linux linux-firmware linux-firmware-intel networkmanager intel-ucode mesa vulkan-intel xf86-video-intel xf86-video-fbdev
