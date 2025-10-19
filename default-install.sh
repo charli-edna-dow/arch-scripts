3adc07e8e11152d84a8ed8e0c0e9d499268b6f78
charli-edna-dow <charli.edna.dow@gmail.com>
2025-10-18 20:05:38 -0400

    Add files via upload

--- default-install.sh
+++ default-install.sh
@@ -1,1 +1,13 @@
-pacman -Syu --needed git xdg-utils xscreensaver xorg xorg-server xorg-xinit lxqt breeze-icons man-db man-docs nano gnome evolution pipewire wireplumber pipewire-jack pipewire-pulse pipewire-alsanoto-fonts noto-fonts-cjk noto-fonts-emoji firefox libreoffice gcc htop sudo
+pacman -S --noconfirm --needed git xdg-utils xscreensaver xorg xorg-server xorg-xinit
+read 
+pacman -S --noconfirm --needed breeze-icons man-db
+read 
+pacman -S --noconfirm --needed man-pages nano pipewire wireplumber pipewire-jack pipewire-pulse pipewire-alsa 
+read
+pacman -S --noconfirm --needed noto-fonts noto-fonts-cjk noto-fonts-emoji firefox libreoffice-fresh gcc htop sudo
+read
+pacman -S --noconfirm --needed lxqt
+read
+pacman -S --noconfirm --needed gnome
+read
+pacman -S --noconfirm --needed evolution
---
+++ default-remove.sh
@@ -0,0 +1,7 @@
+pacman -Rns --noconfirm git xdg-utils xscreensaver xorg xorg-server xorg-xinit breeze-icons man-db man-pages nano pipewire wireplumber pipewire-jack pipewire-pulse pipewire-alsa noto-fonts noto-fonts-cjk noto-fonts-emoji firefox libreoffice-fresh gcc htop sudo
+read
+pacman -Rns --noconfirm lxqt
+read
+pacman -Rns --noconfirm gnome
+read
+pacman -Rns --noconfirm evolution
